a47fdaedfffae28ea115d1b06a7e1d1cc3636c7f
armaninformatics <ammangdeveloper@gmail.com>
2024-04-21 19:18:46 +0800

    getUserById

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,20 @@
 const db = require('../models');
+
 exports.getAllUser = async (req, res) => {
     try {
         const users = await db.User.findAll(); 
         res.json(users.length > 0 ? users : {data: null});
     } catch (error) {
-        console.error('Error:', error);
+        res.status(500).json({ message: error.message });
+    }
+}
+
+exports.getUserById = async (req, res) => {
+    try {
+        const userId = req.params.id
+        const user = await db.User.findByPk(userId)
+        res.json(user ? user : {user: "Not found"})
+    } catch (error) {
         res.status(500).json({ message: error.message });
     }
 }
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,5 +4,6 @@ const router = express.Router()
 const userController = require('../controllers/userController')
 
 router.get('/users', userController.getAllUser)
+router.get('/user/:id', userController.getUserById)
 
 module.exports = router
